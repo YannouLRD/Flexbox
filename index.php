555f3ee72d6e195f2a81863bd8e0546fb7e3c1df
Yann Lenerand <lenerand54@gmail.com>
2022-12-04 14:32:23 +0100

    travail sur les flexbox

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="style.css" />
+    <title>Document</title>
+  </head>
+  <body>
+    <!-- Navbar -->
+    <header>
+        <h1>Mon site</h1>
+      <ul class="nav">
+        <li class="navbar item1"><a href="#"> Accueil</a></li>
+        <li class="navbar item1"><a href="#"> Mes photos</a></li>
+        <li class="navbar item1"><a href="#"> Mon matériel</a></li>
+        <li class="navbar item1"><a href="#"> Me contacter</a></li>
+      </ul>
+    </header>
+    <!-- Fin de narbar -->
+    <!-- Main -->
+    <main>
+      <div class="container">
+        <div class="item"></div>
+        <div class="item"></div>
+        <div class="item"></div>
+        <div class="item"></div>
+        <div class="item"></div>
+        <div class="item"></div>
+      </div>
+    </main>
+    <!-- fin du main -->
+  </body>
+</html>
